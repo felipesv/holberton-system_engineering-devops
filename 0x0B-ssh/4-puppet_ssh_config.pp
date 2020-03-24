3ac33064c0101dcb96496d8264346d89142591a3
felipesv <1165@holbertonschool.com>
2020-03-23 19:24:36 -0500

    add task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Client configuration file (w/ Puppet) 
+file_line { 'Declare identity file':
+  ensure => 'present',
+  line   => ' IdentityFile ~/.ssh/holberton',
+  path   => '/etc/ssh/ssh_config'
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  line   => ' PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  match  => 'PasswordAuthentication yes'
+}
