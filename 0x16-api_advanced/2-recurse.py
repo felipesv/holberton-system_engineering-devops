689844b8de0836b54a367287910ab82377471fbe
felipesv <1165@holbertonschool.com>
2020-05-07 18:49:05 -0500

    add task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Recurse it!"""
+import requests
+
+
+def recurse(subreddit, after="", hot_list=[]):
+    """
+    Recurse it!
+    """
+    if after is None:
+        return []
+
+    headers = {"User-Agent": "wfelipesv"}
+    url = "https://www.reddit.com/r/{}".format(subreddit)
+    url += "/hot.json?limit=100?after={}".format(after)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    # try:
+    if (r.status_code != 200):
+        return None
+    json_data = r.json()
+    for item in json_data.get("data").get("children"):
+        hot_list.append(item.get("data").get("title"))
+    after = json_data.get("data").get("after")
+    return hot_list.extend(recurse(subreddit, after))
+    # except Exception:
+    # return None
